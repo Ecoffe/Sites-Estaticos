64aad30a0ed1bc5ffa15b8caf6a25f92be303c66
NaomiCalisto <naomi.calisto@sptech.school>
2024-06-10 16:56:03 -0300

    Aplicação da atualização dos gráficos de medidas

--- web-data-viz/src/models/medidaModel.js
+++ web-data-viz/src/models/medidaModel.js
@@ -36,10 +36,24 @@ function buscarMedidasEmTempoReal(idEstufa) {
     return database.executar(instrucaoSql);
 }
 
+function atualizacaoUmidade(idEstufa) {
+    var instrucaoSql = `select umidade, DATE_FORMAT(diaHora, '%H:%i') as diaHora from dados join sensor on fkSensor = idSensor where fkSensor = 1 and fkEstufa = ${idEstufa} order by idDados desc limit 1;`;
+
+    return database.executar(instrucaoSql);
+}
+
+function atualizacaoTemperatura(idEstufa) {
+        var instrucaoSql = `select temperatura, DATE_FORMAT(diaHora, '%H:%i') as diaHora from dados join sensor on fkSensor = idSensor where fkSensor = 1 and fkEstufa = ${idEstufa} order by idDados desc limit 1;`;
+
+    return database.executar(instrucaoSql);
+}
+
 module.exports = {
     buscarUltimasMedidasTemperatura,
     buscarUltimasMedidasUmidade,
     listarKpi,
     buscarMensal,
-    buscarMedidasEmTempoReal
+    buscarMedidasEmTempoReal,
+    atualizacaoUmidade,
+    atualizacaoTemperatura
 }
